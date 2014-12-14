fa2e9acb53dd42d8058e7e5cfc89e4d6d610930c
Alec-Langford <alec.langford@hp.com>
2014-12-15 09:38:55 +1100

    Added uniq of file read names

--- sitereviewcmdln.py
+++ sitereviewcmdln.py
@@ -1,5 +1,5 @@
 import sys, os, bs4, simplejson
-import requests, time
+import requests, time,urlparse
 
 
 def check(x):
@@ -17,10 +17,22 @@ def check(x):
     except KeyError:
         return r.content
 
+
+def gsb_check(x):
+    if "http" not in x:
+        x="http%3A%2F%2F"+x
+    url="https://sb-ssl.google.com/safebrowsing/api/lookup?client=demo-app&key=AIzaSyD3kccdlWrye4B_3-PV7sz_DazUOTugZmI&appver=1.5.2&pver=3.1&url="+"%s"%x
+    r=requests.get(url)
+    return bool(r.text)
+
+
+
+
 if __name__ == "__main__":
     t=sys.argv[-1]
     if os.path.isfile(t):
-        for item in open(t, "r").readlines():
+        c=[urlparse.urlparse(item.lower().replace("hxxp","http").strip()).hostname for item in open(t, "r").readlines()]
+        for item in set(c):
             print check(item)
             time.sleep(2)
 
